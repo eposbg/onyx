40c173842710112a06e050ebe5b1cf54970d28fd
Emanuil Stoyanov <emanuil.stoyanov@gmail.com>
2025-04-14 10:32:10 +0100

    Introduce test helpers to deal with the iqueriable error

--- Src/eCommerce/eCommerce.Products.Tests/eCommerce.Products.UnitTests/Application/Services/ProductServiceTests.cs
+++ Src/eCommerce/eCommerce.Products.Tests/eCommerce.Products.UnitTests/Application/Services/ProductServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿using eCommerce.Products.Application.Services.Product;
 using eCommerce.Products.Domain.Entries;
 using eCommerce.Products.Domain.Repositories;
+using eCommerce.Products.UnitTests.Helpers;
 using FluentAssertions;
 using Moq;
 
@@ -10,50 +11,52 @@ public class ProductServiceTests
 {
     private readonly Mock<IProductRepository> _repoMock;
     private readonly ProductService _service;
-    
+
     public ProductServiceTests()
     {
         _repoMock = new Mock<IProductRepository>();
         _service = new ProductService(_repoMock.Object);
     }
-    
+
     [Fact]
-    public async Task GetAll_ShouldReturnAllProducts()
+    public async Task GetAllAsync_ShouldReturnAllProductsAsDtos()
     {
         // Arrange
         var products = new List<Product>
         {
             new() { Id = 1, Name = "Shirt", Color = "Red" },
             new() { Id = 2, Name = "Shoes", Color = "Blue" }
-        }.AsQueryable();
-        _repoMock.Setup(r => r.GetAll()).Returns(products);
+        };
+        var asyncProducts = new TestAsyncEnumerable<Product>(products);
+        _repoMock.Setup(r => r.GetAll()).Returns(asyncProducts);
 
         // Act
         var result = await _service.GetAllAsync(CancellationToken.None);
 
         // Assert
         result.Should().HaveCount(2);
-        result.First().Name.Should().Be("Shirt");
+        result.Should().BeEquivalentTo(products, options =>
+            options.ExcludingMissingMembers());
     }
-    
+
     [Fact]
-    public async Task GetProductsByColor_ShouldReturnFilteredProducts()
+    public async Task GetByColorAsync_ShouldReturnOnlyMatchingColor()
     {
         // Arrange
-        var color = "Red";
         var products = new List<Product>
         {
             new() { Id = 1, Name = "Shirt", Color = "Red" },
-            new() { Id = 2, Name = "Shoes", Color = "Blue" }
-        }.AsQueryable();
-        
-        _repoMock.Setup(r => r.GetAll()).Returns(products);
+            new() { Id = 2, Name = "Shoes", Color = "Blue" },
+            new() { Id = 3, Name = "Hat", Color = "Red" }
+        };
+        var asyncProducts = new TestAsyncEnumerable<Product>(products);
+        _repoMock.Setup(r => r.GetAll()).Returns(asyncProducts);
 
         // Act
-        var result = await _service.GetByColorAsync(color, CancellationToken.None);
+        var result = await _service.GetByColorAsync("Red", CancellationToken.None);
 
         // Assert
-        result.Should().OnlyContain(p => p.Color == "Red");
         result.Should().HaveCount(2);
+        result.Select(p => p.Color).Should().AllBe("Red");
     }
 }
---
+++ Src/eCommerce/eCommerce.Products.Tests/eCommerce.Products.UnitTests/Helpers/TestAsyncEnumerable.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace eCommerce.Products.UnitTests.Helpers;
+
+public class TestAsyncEnumerable<T> : EnumerableQuery<T>, IAsyncEnumerable<T>, IQueryable<T>
+{
+    public TestAsyncEnumerable(IEnumerable<T> enumerable) : base(enumerable) { }
+    public TestAsyncEnumerable(Expression expression) : base(expression) { }
+
+    public IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken = default) =>
+        new TestAsyncEnumerator<T>(this.AsEnumerable().GetEnumerator());
+
+    IQueryProvider IQueryable.Provider => new TestAsyncQueryProvider<T>(this);
+}
